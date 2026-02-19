9eff4953d2b212c45028faae536c6005d1b1d30b
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-20 00:52:09 +0530

    feat: Selection Sorting

---
+++ src/com/java/sorting/SelectionSorting.java
@@ -0,0 +1,35 @@
+package com.java.sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSorting {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+        sc.close();
+    }
+}
+
+
